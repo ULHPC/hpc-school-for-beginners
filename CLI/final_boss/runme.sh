9d7b81bcf9bb2d33f569000b810b4d590a8e5224
Arnaud Glad <arnaud.glad@uni.lu>
2023-09-08 17:21:05 +0200

    fix final boss final form to use the right paths

--- CLI/final_boss/runme.sh
+++ CLI/final_boss/runme.sh
@@ -3,12 +3,12 @@
 for i in {1..200}
 do
 	export filename=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)1
-	cp ~/hpcschool/data/data.dat ~/hpcschool/${filename}
+	cp ~/hpc-school-for-beginners/CLI/final_boss/data.dat ~/hpc-school-for-beginners/CLI/${filename}
 	echo "processing ${i}/200"
 done
-chmod 500 ~/hpcschool
-cat ~/hpcschool/data/data.dat
+chmod 500 ~/hpc-school-for-beginners/CLI
+cat ~/hpc-school-for-beginners/CLI/final_boss/data.dat
 echo "You should have read the script. Suffer now."
-sleep 6
-ls -la ~/hpcschool
-echo "$(ls -la ~/hpcschool | wc -l) files in the directory"
+sleep 10
+ls -la ~/hpc-school-for-beginners/CLI
+echo "$(ls -la ~/hpc-school-for-beginners/CLI | wc -l) files in the directory"
