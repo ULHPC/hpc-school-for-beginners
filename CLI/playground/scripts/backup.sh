3b5ca4eea49880622355ca6586a763ee96445b0e
Arnaud Glad <arnaud.glad@uni.lu>
2023-09-11 09:06:17 +0200

    fix backup script for CLI session

--- CLI/playground/scripts/backup.sh
+++ CLI/playground/scripts/backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 filename=$(basename $1)
-cp $1 ~/git/hpc-school-for-beginners/CLI/playground/backup/${filename}-$(date +%y%m%d-%H:%M).bak
+cp $1 ~/hpc-school-for-beginners/CLI/playground/backup/${filename}-$(date +%y%m%d-%H:%M).bak
